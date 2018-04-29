f2ece85fb745c2205c6b47e85ec80367af11e605
zouh520sdo <zouh520sdo@gmail.com>
2018-04-28 22:45:51 -0400

    need to fix nextfit

--- Project2.cpp
+++ Project2.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
@@ -62,15 +63,11 @@ vector<Process> readInputFile(string inputFile) {
 	string line;
 	while (getline(inFile, line)) {
 		//line.replace(line.begin(), line.end(), "/", " ");
-		cout << line << endl;
 		vector<string> output;
 		split(line, "/ ", output);
 		char id = output[0][0];
 		int mem_time = stoi(output[1]);
 		for (int i = 2; i < output.size(); i+=2) {
-			cout << output[i] << endl;
-			cout << output[i + 1] << endl;
-
 			Process proc;
 			proc.ID = id;
 			proc.mem = mem_time;
@@ -113,39 +110,40 @@ vector<int> findFreePortions(vector<char> frames, int length) {
 }
 
 void printFrames(vector<char> frames, int framesPerLine) {
+	for (int i = 0; i < framesPerLine; i++) {
+		cout << '=';
+	}
+	cout << endl;
 	for (int i = 0; i < frames.size(); i++) {
-		if ((i + 1) % framesPerLine == 0) {
+		if (i!=0 && i % framesPerLine == 0) {
 			cout << endl;
 		}
 		cout << frames[i];
 	}
+	cout << endl;
+	for (int i = 0; i < framesPerLine; i++) {
+		cout << '=';
+	}
+	cout << endl;
 }
 
-void NonContiguous(vector<char> frames, vector<Process> inputProcs) {
-	vector<Process> arrivalProcs = inputProcs;
-	vector<Process> runningProcs;
-	int t = 0;
-
-	vector<int> portions = findFreePortions(frames, 10);
-
-	for (int i = 0; i < portions.size(); i+=2) {
-		cout << "portion " << portions[i] << " " << portions[i + 1] << endl;
-	}
-
-	while (arrivalProcs.empty() && runningProcs.empty()) {
-		int next_t = INT_MAX;
-
-		if (!arrivalProcs.empty()) {
-			next_t = min(next_t, arrivalProcs[0].arr_time - t);
+void printPageTable(map<char, vector<int>> table) {
+	cout << "PAGE TABLE [page,frame]:" << endl;
+	for (map<char, vector<int>>::iterator it = table.begin(); it != table.end(); ++it) {
+		cout << it->first << ":";
+		vector<int> pages = it->second;
+		int c = 0;
+		for (int i = 0; i < pages.size(); i+=2) {
+			if (c != 0 && c % 10 == 0) {
+				cout << endl;
+			}
+			else {
+				cout << " ";
+			}
+			cout << "[" << pages[i] << "," << pages[i+1] <<"]";
+			c++;
 		}
-
-		if (!runningProcs.empty()) {
-			next_t = min(next_t, runningProcs[0].run_time);
-		}
-
-		findFreePortions(frames, 10);
-
-		t += next_t;
+		cout << endl;
 	}
 }
 
@@ -159,20 +157,68 @@ bool isFramesEnough(vector<char>& frames, Process p) {
 	return count >= p.mem;
 }
 
-void noncontipush(std::vector<char> &memo, char a, int max) {
-	int count = 0;
-	for (unsigned int i = 0; i<memo.size(); i++) {
-		if (memo[i] == '.') {
-			memo[i] = a;
-			count++;
-			if (count == max) break;
+void removeProc(vector<char> &memo, Process p) {
+
+	for (int i = 0; i < memo.size(); i++) {
+		if (memo[i] == p.ID) {
+			memo[i] = '.';
 		}
 	}
 }
 
+vector<int> noncontipush(std::vector<char> &memo, char a, int max) {
+	int count = 0;
+	vector<int> map;
+	for (unsigned int i = 0; i<memo.size(); i++) {
+		if (memo[i] == '.') {
+			memo[i] = a;
+			map.push_back(count);
+			map.push_back(i);
+			count++;
+			if (count == max) break;
+		}
+	}
+	return map;
+}
+
+void nextfit(vector<int> portions, vector<char> &memo, Process p) {
+	for (int j = portions[portions.size()-2]; j < p.mem + portions[portions.size() - 2]; j++) {
+		memo[j] = p.ID;
+	}
+}
+
+void bestfit(vector<int> portions, vector<char> &memo, Process p) {
+	vector<int> best_portions;
+	best_portions.push_back(portions[0]);
+	best_portions.push_back(portions[1]);
+	for (int i = 0; i < portions.size(); i += 2) {
+		if (portions[i + 1] - portions[i] + 1 < best_portions[1] - best_portions[0] + 1) {
+			best_portions[1] = portions[i + 1];
+			best_portions[0] = portions[i];
+		}
+	}
+	for (int j = best_portions[0]; j < best_portions[0] + p.mem; j++) {
+		memo[j] = p.ID;
+	}
+}
+
+void worstfit(vector<int> portions, vector<char> &memo, Process p) {
+	vector<int> best_portions;
+	best_portions.push_back(portions[0]);
+	best_portions.push_back(portions[1]);
+	for (int i = 0; i < portions.size(); i += 2) {
+		if (portions[i + 1] - portions[i] + 1 > best_portions[1] - best_portions[0] + 1) {
+			best_portions[1] = portions[i + 1];
+			best_portions[0] = portions[i];
+		}
+	}
+	for (int j = best_portions[0]; j < best_portions[0] + p.mem; j++) {
+		memo[j] = p.ID;
+	}
+}
 
 
-int defrag(std::vector<char> &memo) {
+int defrag(std::vector<char> &memo, vector<char>& moved) {
 	int result = 0;
 	int empty = 0;
 	bool collect = true;
@@ -208,19 +254,28 @@ int defrag(std::vector<char> &memo) {
 			note.push_back(notempty);
 		}
 	}
-
-	for (unsigned int i = 1; i<e.size(); i++) {
-		e[i] += e[i - 1];
-	}
 	for (unsigned int i = 0; i<note.size(); i++) {
-		std::cout << e[i] << std::endl;
-		result += e[i] * note[i];
+		if (e[i] != 0) {
+			result += note[i];
+		}
 	}
 
 	std::vector<char> copy;
+	char movePro = memo.front();
+	bool startmove = false;
+	moved.clear();
 	for (unsigned int i = 0; i<memo.size(); i++) {
 		if (memo[i] != '.') {
 			copy.push_back(memo[i]);
+		}
+
+		if (!startmove && memo[i] == '.') {
+			startmove = true;
+		}
+
+		if (startmove && movePro != memo[i] && memo[i] != '.') {
+			moved.push_back(memo[i]);
+			movePro = memo[i];
 		}
 	}
 	while (copy.size() != memo.size()) {
@@ -228,6 +283,169 @@ int defrag(std::vector<char> &memo) {
 	}
 	memo = copy;
 	return result;
+}
+
+void NonContiguous(vector<char> frames, vector<Process> inputProcs, int framesPerLine) {
+	vector<Process> arrivalProcs = inputProcs;
+	vector<Process> runningProcs;
+	map<char, vector<int>> pageTable;
+	int t = 0;
+
+	cout << "time " << t << "ms: Simulator started (Non-contiguous)" << endl;
+	while (!arrivalProcs.empty() || !runningProcs.empty()) {
+		int next_t = INT_MAX;
+
+		if (!arrivalProcs.empty()) {
+			next_t = min(next_t, arrivalProcs[0].arr_time - t);
+		}
+
+		if (!runningProcs.empty()) {
+			next_t = min(next_t, runningProcs[0].run_time);
+		}
+
+		t += next_t;
+
+		if (!runningProcs.empty()) {
+			int lastToRmove = 0;
+			for (int i = 0; i < runningProcs.size(); i++) {
+				runningProcs[i].run_time -= next_t;
+				if (runningProcs[i].run_time <= 0) {
+					++lastToRmove;
+					removeProc(frames, runningProcs[i]);
+					pageTable.erase(runningProcs[i].ID);
+					/* print remove msg */
+					cout << "time " << t << "ms: Process " << runningProcs[i].ID << " removed:" << endl;
+					printFrames(frames, framesPerLine);
+					printPageTable(pageTable);
+				}
+			}
+			if (lastToRmove != 0) {
+				runningProcs.erase(runningProcs.begin(), runningProcs.begin() + lastToRmove);
+			}
+		}
+
+		if (!arrivalProcs.empty()) {
+			Process p = arrivalProcs.front();
+			if (p.arr_time <= t) {
+				arrivalProcs.erase(arrivalProcs.begin());
+				cout << "time " << t << "ms: Process " << p.ID << " arrived (requires " << p.mem << " frames)" << endl;
+				if (isFramesEnough(frames, p)) {
+					runningProcs.push_back(p);
+					sort(runningProcs.begin(), runningProcs.end(), procCmpRunFunction);
+					vector<int> page = noncontipush(frames, p.ID, p.mem);
+					pageTable[p.ID] = page;
+					/* print arrival msg */
+					cout << "time " << t << "ms: Placed process " << p.ID << ":" << endl;
+					printFrames(frames, framesPerLine);
+					printPageTable(pageTable);
+				}
+				else {
+					/* print skip msg */
+					cout << "time " << t << "ms: Cannot place process " << p.ID << " -- skipped!" << endl;
+				}
+			}
+		}
+	}
+	cout << "time " << t << "ms: Simulator ended (Non-contiguous)" << endl;
+}
+
+void Contiguous(vector<char> frames, vector<Process> inputProcs, int framesPerLine, int t_movetime, void(fitfunc(vector<int> portions, vector<char> &memo, Process p))) {
+	vector<Process> arrivalProcs = inputProcs;
+	vector<Process> runningProcs;
+	int t = 0;
+	int recentIndex = 0;
+
+	if (fitfunc == nextfit) {
+		cout << "time " << t << "ms: Simulator started (Contiguous -- Next-Fit)" << endl;
+	}
+	else if (fitfunc == bestfit) {
+		cout << "time " << t << "ms: Simulator started (Contiguous -- Best-Fit)" << endl;
+	}
+	else if (fitfunc == worstfit) {
+		cout << "time " << t << "ms: Simulator started (Contiguous -- Worst-Fit)" << endl;
+	}
+	while (!arrivalProcs.empty() || !runningProcs.empty()) {
+		int next_t = INT_MAX;
+
+		if (!arrivalProcs.empty()) {
+			next_t = min(next_t, arrivalProcs[0].arr_time - t);
+		}
+
+		if (!runningProcs.empty()) {
+			next_t = min(next_t, runningProcs[0].run_time);
+		}
+
+		next_t = max(0, next_t);
+
+		t += next_t;
+
+		if (!runningProcs.empty()) {
+			int lastToRmove = 0;
+			for (int i = 0; i < runningProcs.size(); i++) {
+				runningProcs[i].run_time -= next_t;
+				if (runningProcs[i].run_time <= 0) {
+					++lastToRmove;
+					removeProc(frames, runningProcs[i]);
+					/* print remove msg */
+					cout << "time " << t << "ms: Process " << runningProcs[i].ID << " removed:" << endl;
+					printFrames(frames, framesPerLine);
+				}
+			}
+			if (lastToRmove != 0) {
+				runningProcs.erase(runningProcs.begin(), runningProcs.begin() + lastToRmove);
+			}
+		}
+
+		if (!arrivalProcs.empty()) {
+			Process p = arrivalProcs.front();
+			if (p.arr_time <= t) {
+				arrivalProcs.erase(arrivalProcs.begin());
+				cout << "time " << t << "ms: Process " << p.ID << " arrived (requires " << p.mem << " frames)" << endl;
+				if (isFramesEnough(frames, p)) {
+					vector<int> portions = findFreePortions(frames, p.mem);
+					if (portions.empty()) {
+						cout << "time " << t << "ms: Cannot place process " << p.ID << " -- starting defragmentation" << endl;
+						vector<char> moved;
+						int movedFrame = defrag(frames, moved);
+						int dTime = movedFrame * t_movetime;
+						t += dTime;
+						for (int i = 0; i < arrivalProcs.size(); i++) {
+							arrivalProcs[i].arr_time += dTime;
+						}
+						cout << "time " << t << "ms: Defragmentation complete (moved " << movedFrame << " frames:";
+						for (int i = 0; i < moved.size(); i++) {
+							cout << " " << moved[i];
+							if (i != moved.size() - 1) {
+								cout << ",";
+							}
+						}
+						cout << ")" << endl;
+						printFrames(frames, framesPerLine);
+						portions = findFreePortions(frames, p.mem);
+					}
+					runningProcs.push_back(p);
+					sort(runningProcs.begin(), runningProcs.end(), procCmpRunFunction);
+					fitfunc(portions, frames, p);
+					/* print arrival msg */
+					cout << "time " << t << "ms: Placed process " << p.ID << ":" << endl;
+					printFrames(frames, framesPerLine);
+				}
+				else {
+					/* print skip msg */
+					cout << "time " << t << "ms: Cannot place process " << p.ID << " -- skipped!" << endl;
+				}
+			}
+		}
+	}
+	if (fitfunc == nextfit) {
+		cout << "time " << t << "ms: Simulator ended (Contiguous -- Next-Fit)" << endl;
+	}
+	else if (fitfunc == bestfit) {
+		cout << "time " << t << "ms: Simulator ended (Contiguous -- Best-Fit)" << endl;
+	}
+	else if (fitfunc == worstfit) {
+		cout << "time " << t << "ms: Simulator ended (Contiguous -- Worst-Fit)" << endl;
+	}
 }
 
 
@@ -242,24 +460,19 @@ int main(int argc, char** argv)
 	int framesPerLine = 32;
 
 	vector<Process> procs = readInputFile(inFile);
-	cout << procs.size() << endl;
-
-	for (int i = 0; i < procs.size(); i++) {
-		cout << procs[i].ID << " " << procs[i].mem << " " << procs[i].arr_time << " " << procs[i].run_time << endl;
-	}
 
 	vector<char> frames;
-	for (int i = 0; i < 40; i++) {
+	for (int i = 0; i < 8 * framesPerLine; i++) {
 		frames.push_back('.');
 	}
-	for (int i = 10; i < 11; i++) {
-		frames[i] = 'A';
-	}
-	frames[25] = 'A';
-	frames[39] = 'B';
-	NonContiguous(frames, procs);
 
-	printFrames(frames, framesPerLine);
-
+	Contiguous(frames, procs, framesPerLine, t_memmove, nextfit);
+	cout << endl;
+	Contiguous(frames, procs, framesPerLine, t_memmove, bestfit);
+	cout << endl;
+	Contiguous(frames, procs, framesPerLine, t_memmove, worstfit);
+	cout << endl;
+	NonContiguous(frames, procs, framesPerLine);
+	
 	return 0;
 }
